675d6ee893593dd8b07e3aba62dad24ab30e39f5
mathfh <116044517+mathfh@users.noreply.github.com>
2022-12-16 16:00:20 -0300

    Add files via upload

---
+++ imput/imput.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>enviar</title>
+
+</head>
+<body>
+<form method="POST"> 
+<input type="number" name="quantidade"> 
+<input type="submit" name="enviar">
+
+
+</form>
+<nav> 
+
+<?php
+
+$receber = $_POST['quantidade'];
+echo $receber;
+$mat[5] [5];
+$i; $j; $tam = 5;
+
+
+for($i = 0; $i < $tam; $i++) {
+    for($j = 0; $j < $tam; $j++){
+        $mat[$i][$j] = $i+$j;
+
+    }
+}
+return 0;
+?>
+    <a href=""download>Download</a> 
+</form>
+</nav>
+</body>
+</html>
